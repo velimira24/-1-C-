b85419c3f217e58fd4198564c62ea96bdb062b01
Velimira <revekka-best@mail.ru>
2022-12-01 02:18:27 +0300

    Homework

---
+++ Задача 6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console. WriteLine("Программа определит, является ли число четным");
+Console. WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0)
+
+Console. WriteLine("четное");
+    
+else 
+Console. WriteLine("нечетное");
