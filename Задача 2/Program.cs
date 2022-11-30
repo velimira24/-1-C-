6be65633a830dd7193285d50a1a52faf3804ff1d
Velimira <revekka-best@mail.ru>
2022-12-01 01:04:32 +0300

    Homework

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,6 +1,10 @@
-﻿int a = -9;
-int b = -3;
-int max = a;
-if (b>max) max =b;
+﻿Console. WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console. WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num2>max) max =num2;
 Console. Write ("max=");
 Console.WriteLine(max);
