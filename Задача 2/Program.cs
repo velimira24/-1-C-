972b924dc36c83a7ba33add27bbccbadad7fe29c
Velimira <revekka-best@mail.ru>
2022-12-01 01:41:51 +0300

    homework

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -4,7 +4,10 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console. WriteLine("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-int max = num1;
-if (num2>max) max =num2;
-Console. Write ("max=");
+int max = num1; 
+int min =num2;
+if (num2>max) max =num2;min =num1;
+Console. Write ("max = ");
 Console.WriteLine(max);
+Console.Write ("min = ");
+Console.WriteLine(min);
