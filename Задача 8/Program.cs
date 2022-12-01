822eb091e09383a22ef5c271cf684160a3220f7f
Velimira <revekka-best@mail.ru>
2022-12-02 00:37:43 +0300

    Homework

---
+++ Задача 8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int rest =0;
+while (rest<num1-1)
+{
+
+    rest = rest + 2;
+Console.Write(rest);
+Console.Write(',');
+}
