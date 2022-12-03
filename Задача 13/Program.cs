fdd41e3289ffbc562f3d1f7d39e9de61462042fc
Velimira <revekka-best@mail.ru>
2022-12-03 23:16:39 +0300

    homework

---
+++ Задача 13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse (Console.ReadLine());
+string chr = Convert.ToString(num);
+if (num > 99)
+{
+   System.Console.WriteLine(chr[2]); 
+}
+else
+{ System.Console.WriteLine("третьей цифры нет");
+}
