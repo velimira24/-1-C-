5e3f629d7b69f9a8fea93f5f16839f6177c44ee7
Velimira <revekka-best@mail.ru>
2022-12-03 22:48:57 +0300

    homework

---
+++ Задача 10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+
+int num = int.Parse(Console.ReadLine());
+if (num > 999 || num < 100)
+{
+    Console.WriteLine("Это число не трехзначное");
+}
+else
+{
+    Console.WriteLine((num / 10)% 10);
+}
