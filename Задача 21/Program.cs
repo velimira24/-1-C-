b0387aa17350e8915d962381e6a639958527fdaa
Velimira <revekka-best@mail.ru>
2022-12-06 23:25:23 +0300

    homework

---
+++ Задача 21/Program.cs
@@ -0,0 +1,15 @@
+﻿int[]point = new int[6];
+Console.WriteLine ("Введите X1: ");
+point[0]=int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите Y1: ");
+point[1]=int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите Z1: ");
+point[2]=int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите X2: ");
+point[3]=int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите Y2: ");
+point[4]=int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите Z2: ");
+point[5]=int.Parse(Console.ReadLine());
+
+Console.WriteLine(MathF.Sqrt((float)(Math.Pow(point[3] - point[0],2) + Math.Pow(point[4] - point[1],2) + Math.Pow(point[5] - point[2],2))));
