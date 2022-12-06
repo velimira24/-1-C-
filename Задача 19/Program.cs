a4a22fa2eb759dadb2eeda9067a25ec9eeeb61fe
Velimira <revekka-best@mail.ru>
2022-12-06 22:24:25 +0300

    homework

---
+++ Задача 19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string n = Console.ReadLine();
+int len = n.Length;
+
+if (len == 5)
+{
+    if (n[0] == n[4] && n[1] == n[3])
+    {
+        Console.WriteLine("Палиндром");
+    }
+    else
+    {
+        Console.WriteLine(" Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Число не является пятизначным");
+}
