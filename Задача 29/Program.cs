c185e2d1bc0404d547bffd2e5eb4e55446ce948f
Velimira <revekka-best@mail.ru>
2022-12-11 23:01:27 +0300

    homework

---
+++ Задача 29/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = Auto();
+
+System.Console.WriteLine(string.Join(" | ", arr));
+
+int[] Auto()
+{
+int[] arr = new int[8];
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(0,100);
+}
+return arr;
+}
