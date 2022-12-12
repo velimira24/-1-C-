1e4484b0f2129fb16769ce1eaddbe00733f88cd6
Velimira <revekka-best@mail.ru>
2022-12-12 09:23:35 +0300

    homework

--- Задача 27/Program.cs
+++ Задача 27/Program.cs
@@ -1,10 +1,18 @@
 ﻿Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int result = 0;
+
+
+int result = Sum(n);
+
+System.Console.WriteLine($"Сумма цифр в веденном числе = {result}");
+
+int Sum(int number)
+{int result = 0;
 while(n > 0)
 {
     result = result + n % 10;
     n = n / 10;
 
 }
-System.Console.WriteLine($"Сумма цифр в веденном числе = {result}");
+return result;
+}
