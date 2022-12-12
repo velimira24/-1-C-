49dc4fb6711d0a306cadee7b8352acc269397854
Velimira <revekka-best@mail.ru>
2022-12-12 10:14:34 +0300

    homework

---
+++ Задача 25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = Degree(A, B);
+
+System.Console.WriteLine(result);
+
+int Degree(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    } 
+    return result; 
+
+}
