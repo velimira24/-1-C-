62d65c29db212b7fa8d97d9878b3211da5917529
Velimira <revekka-best@mail.ru>
2022-12-14 21:56:47 +0300

    homework

---
+++ Задача 34/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] numbers = new int[4];
+RandonNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+
+void RandonNumbers(int[] numbers)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
+
+int count = 0;
+
+for (int x = 0; x < numbers.Length; x++)
+{
+if (numbers[x] % 2 == 0)
+count++;
+}
+Console.WriteLine($"из {numbers.Length} чисел, {count} четных");
+
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
