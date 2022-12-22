3ccbe05f1d7fa00db53b385c2ee1cb9b67f9c7b8
Velimira <revekka-best@mail.ru>
2022-12-22 08:07:00 +0300

    homework

---
+++ Задача 50/Program.cs
@@ -0,0 +1,77 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+//  void FiiArray(int[,]col)
+// {
+// Random rand = new Random();
+// Console.WriteLine("Введите количество строк");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[,] arr = new int[n, m];
+// Console.WriteLine("Исходный массив: ");
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arr.GetLength(1); j++)
+// {
+//     arr[i, j] = rand.Next(10, 99);
+// Console.Write("{0} ", arr[i, j]);
+// }
+// Console.WriteLine();
+// }
+// }
+
+void PrintArray(int[,] matr)
+{
+ for (int i = 0; i < matr.GetLength(0); i++)
+ {
+ for (int j = 0; j < matr.GetLength(1); j++)
+ {
+ Console.Write($"{matr[i, j]} ");
+ }
+ Console.WriteLine();
+ }
+}
+void FillArray(int[,] matr)
+{
+ for (int i = 0; i < matr.GetLength(0); i++)
+ {
+ for (int j = 0; j < matr.GetLength(1); j++)
+ {
+ matr[i,j] = new Random().Next(1,10);
+ }
+ }
+}
+int[,] matrix = new int[4, 5];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+Console.Write("Введите строку: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+
+
+    if (pos1 < 0 | pos1 > matrix.GetLength(0) - 1 | pos2 < 0 | pos2 > matrix.GetLength(1) - 1)
+{
+Console.WriteLine("Элемент не существует  ");
+}
+    else
+{
+    Console.WriteLine("Значение элемента массива = {0}", matrix[pos1, pos2]);
+}
+Console.ReadLine();
