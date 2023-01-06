28a87f3ffb5a8c49de37323bb0a0d6b043c0f970
Velimira <revekka-best@mail.ru>
2023-01-06 11:45:08 +0300

    help

--- inputnumber/Program.cs
+++ inputnumber/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputNumber2()
+ {
+ int num1;
+string text;
+
+while(true)
+{
+    Console.Write("Введите число:");
+     text = Console.ReadLine();
+    if(int.TryParse(text,out num1))
+   {
+        break;
+   }
+   System.Console.WriteLine("Не удалось распознать число.Попробуйте еще раз))");
+ }
+return num1;
+ }
