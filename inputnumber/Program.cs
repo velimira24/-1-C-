b8c59689b94447100dcd479f894100b5c710c534
velimira24 <119067802+velimira24@users.noreply.github.com>
2023-01-08 14:43:33 +0300

    Update Program.cs

--- inputnumber/Program.cs
+++ inputnumber/Program.cs
@@ -51,3 +51,32 @@ void func(int num)
 int[] array_copy = arr[];
 
 int[] array_copy = arr[];//копируем массив как бы по ссылке, то есть, если вначале скопировать массив, а потом что-то в нем изменить, то изменятся числа в скопированном до этого массиве
+
+
+Создать двумерный массив:
+
+void FillArrayRandomNumbers2(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+Напечатать двумерный массив:
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
